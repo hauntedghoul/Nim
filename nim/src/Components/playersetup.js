c209e6bb428c0cd2fdf8d2ecc43313ad9f71943d
ChelseaBui11 <141456665+ChelseaBui11@users.noreply.github.com>
2024-07-02 08:51:57 -0600

    Update Setup page

--- nim/src/Components/playersetup.js
+++ nim/src/Components/playersetup.js
@@ -6,6 +6,13 @@ const playersetup = () => {
     return (
         <div>
             <Nav />
+            <h1>Set up the Game</h1>
+            <input type="text" placeholder="Player 1"></input>
+            <input type="text" placeholder="Player 2"></input>
+
+            <button src="/easy">Easy</button>
+            <button src="/medium">Medium</button>
+            <button src="/hard">Hard</button>
         </div>
     )
 }
