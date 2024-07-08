8584972eade3d4869c8ed1e3dfc6caac543ca2a9
ChelseaBui11 <141456665+ChelseaBui11@users.noreply.github.com>
2024-07-08 08:06:05 -0600

    Added alt text and class so that the css doesn't combat with the other images

--- nim/src/Components/easy.js
+++ nim/src/Components/easy.js
@@ -19,38 +19,36 @@ const Easy = () => {
             </div>
             <div>
                 <div className='grid-container'>
-                    <div className='grid-item'><img src="/images/Roomba.png" /><div>
-                    </div>
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        <div className='grid-container2'>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                        </div>
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        <div className='grid-container3'>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                            <div className='grid-item'><img src="/images/Roomba.png" /></div>
-                        </div>
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        <h1>Current Player: {isFirstPlayer ? 'Player 1' : 'Player 2'}</h1>
-                        <button onClick={nextPlayer}>Next Player</button>
-                    </div>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
                 </div>
-
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className='grid-container2'>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className='grid-container3'>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                    <div className='grid-item'><img src="/images/Roomba.png" class="Roomba" alt="Roomba" /></div>
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <h1>Current Player: {isFirstPlayer ? 'Player 1' : 'Player 2'}</h1>
+                <button onClick={nextPlayer}>Next Player</button>
             </div>
         </div>
+
     );
 }
 
