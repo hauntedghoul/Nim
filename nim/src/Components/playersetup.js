f43c98c964a97fadd6fa7aaf53a908c534c8ed1f
gator <lunruh946@gmail.com>
2024-07-09 11:02:52 -0600

    Update playersetup.js

--- nim/src/Components/playersetup.js
+++ nim/src/Components/playersetup.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Nav from './Navbar/Nav'
-import './playersetup.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Nav from './Navbar/Nav';
+import './playersetup.css';
 
-const playersetup = () => {
+const PlayerSetup = () => {
+    const [playerOne, setPlayerOne] = useState('');
+    const [playerTwo, setPlayerTwo] = useState('');
+
     return (
         <div>
-            <img className='setup' src='/images/SetupWrapper.png' />
-            <br/>
-            <Link to="/"><img className='home-image' src='/images/Home.png' /></Link>
-            <br/>
-            <input className='playerOne' type="text" placeholder="Player 1"></input>
-            <input className='playerTwo' type="text" placeholder="Player 2"></input>
-
-            <br/>
-            <Link to="/easy"><img className='setup-image' src='/images/Easy.png' /></Link>
-            <Link to="/medium"><img className='setup-image' src='/images/Medium.png' /></Link>
-            <Link to="/hard"><img className='setup-image' src='/images/Hard.png' /></Link>
-
+            <img className='setup' src='/images/SetupWrapper.png' alt='Setup Wrapper' />
+            <br />
+            <Link to="/"><img className='home-image' src='/images/Home.png' alt='Home' /></Link>
+            <br />
+            <input 
+                className='playerOne' 
+                type="text" 
+                placeholder="Player 1" 
+                value={playerOne}
+                onChange={(e) => setPlayerOne(e.target.value)}
+            />
+            <input 
+                className='playerTwo' 
+                type="text" 
+                placeholder="Player 2" 
+                value={playerTwo}
+                onChange={(e) => setPlayerTwo(e.target.value)}
+            />
+            <br />
+            <Link to={{ pathname: "/easy", state: { playerOne, playerTwo } }}>
+                <img className='setup-image' src='/images/Easy.png' alt='Easy' />
+            </Link>
+            <Link to={{ pathname: "/medium", state: { playerOne, playerTwo } }}>
+                <img className='setup-image' src='/images/Medium.png' alt='Medium' />
+            </Link>
+            <Link to={{ pathname: "/hard", state: { playerOne, playerTwo } }}>
+                <img className='setup-image' src='/images/Hard.png' alt='Hard' />
+            </Link>
         </div>
-    )
+    );
 }
 
-export default playersetup
+export default PlayerSetup;
